e8905d7f9d10f9cec3db353838adc397f457c704
Justin Vititoe <jvit1985@yahoo.com>
2022-04-28 18:41:38 -0500

    completed initial employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,4 +6,9 @@ test('creates an employee object', () => {
     expect(employee.name).toBe('Justin');
     expect(employee.id).toEqual('25');
     expect(employee.email).toEqual('jvit85@gmail.com');
+
+    expect(employee.getName()).toEqual('Justin');
+    expect(employee.getId()).toEqual('25');
+    expect(employee.getEmail()).toEqual('jvit85@gmail.com');
+    expect(employee.getRole()).toEqual('Employee');
 });

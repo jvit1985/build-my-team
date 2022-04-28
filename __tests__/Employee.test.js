e2f77db7ee4c749eff3f1a8a8bd77a3326862474
Justin Vititoe <jvit1985@yahoo.com>
2022-04-28 18:29:44 -0500

    added employee test parameters to pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,6 +4,6 @@ test('creates an employee object', () => {
     const employee = new Employee('Justin');
 
     expect(employee.name).toBe('Justin');
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.id).toEqual('25');
     expect(employee.email).toEqual('jvit85@gmail.com');
 });
